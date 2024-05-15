b8ffd747a66647aa4626b806689a569ec583b770
Joseph Szpigiel <josephszpigiel@gmail.com>
2024-05-14 21:04:00 -0400

    Finished CRUD functions and initialized express server

--- server/database.js
+++ server/database.js
@@ -10,29 +10,64 @@ const pool = mysql.createPool({
     database: process.env.MYSQL_DATABASE
 }).promise()
 
-export async function getUsers(){
-    const [rows] = await pool.query('SELECT * FROM users;')
-    return rows
+
+//Customer CRUD
+export async function getCustomers(){
+    const [customers] = await pool.query('SELECT * FROM customers;')
+    return customers
 }
 
-export async function getUser(id){
-    const [user] = await pool.query(`
+export async function getCustomer(id){
+    const [customer] = await pool.query(`
     SELECT * 
-    FROM users 
+    FROM customers 
     WHERE id = ?
     `, [id])
-    return user[0]
+    return customer[0]
 }
 
-export async function createUser(name, email){
-    const result = pool.query(`
-    INSERT INTO users (name, email)
-    VALUES ?, ?
-    `, [name, email])
-    const id = result.insertId
-    return getUser(id)
+export async function createCustomer(
+    email,
+    first_name = null,
+    last_name= null,
+    img_url=null,
+    customer_type_id=1
+    ){
+        const [result] = await pool.query(`
+            INSERT INTO customers (first_name, last_name, email, img_url, customer_type_id)
+            VALUES (?, ?, ?, ?, ?)
+            ;`, [first_name, last_name, email, img_url, customer_type_id])
+        const id = result.insertId
+        console.log(id)
+        const added = await getCustomer(id)
+        return added
+    }
+
+export async function updateCustomer(
+    id,
+    email,
+    first_name,
+    last_name,
+    img_url=null,
+    customer_type_id=1
+    ){
+        const [result] = await pool.query(`
+            UPDATE customers
+            SET first_name = ?, last_name = ?, email = ?, img_url = ?, customer_type_id =?
+            WHERE id = ?
+            ;`,  [first_name, last_name, email, img_url, customer_type_id, id])
+        console.log(result)
+        const updated = await getCustomer(id)
+        return updated
+    }
+
+export async function deleteCustomer(id){
+    const [result] = await pool.query(`
+        DELETE FROM customers WHERE id = ?
+    ;`, [id])
+    console.log(result)
+    return result.affectedRows
 }
 
-
-const rows = await getUsers()
-console.log(rows)
+const deleted = await deleteCustomer(11)
+console.log(deleted)
